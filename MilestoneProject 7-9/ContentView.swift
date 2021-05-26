158651b472c3bc16a1369a4481d6c401ad08bc90
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-05-26 22:44:31 +0545

    Initial Commit

---
+++ MilestoneProject 7-9/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MilestoneProject 7-9
+//
+//  Created by Anisha Lamichhane on 5/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MilestoneProject 7-9/MilestoneProject_7_9App.swift
@@ -0,0 +1,17 @@
+//
+//  MilestoneProject_7_9App.swift
+//  MilestoneProject 7-9
+//
+//  Created by Anisha Lamichhane on 5/26/21.
+//
+
+import SwiftUI
+
+@main
+struct MilestoneProject_7_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
